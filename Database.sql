641cc0494a9869c7f060adfd1a08307657214128
maurozappa <fabian_mueller7@sluz.ch>
2022-05-25 13:50:09 +0200

    Fabian Mueller + Mauro Zappa; 10min; Datenbankerstellungsskript

--- Database.sql
+++ Database.sql
@@ -1,49 +1,58 @@
-CREATE TABLE `Spieler`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Vorname` VARCHAR(255) NOT NULL,
-    `Nachname` VARCHAR(255) NOT NULL,
-    `Geburtsdatum` DATE NOT NULL,
-    `Position` VARCHAR(255) NOT NULL,
-    `Nationalität` VARCHAR(255) NOT NULL
+﻿use master
+go
+drop database if exists Fussballspieler
+go
+create database Fussballspieler
+go
+use Fussballspieler
+go
+
+CREATE TABLE Spieler(
+    id INT   identity PRIMARY KEY,
+    Vorname VARCHAR(255) ,
+    Nachname VARCHAR(255) ,
+    Geburtsdatum DATE ,
+    Position VARCHAR(255) ,
+    Nationalität VARCHAR(255) 
 );
-CREATE TABLE `Fifa Ranking`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Fifa Version` VARCHAR(255) NOT NULL,
-    `Gesamt Rating` INT NOT NULL,
-    `Pace Rating` INT NOT NULL,
-    `Shoot Rating` INT NOT NULL,
-    `Pasing Rating` INT NOT NULL,
-    `Dribbling Rating` INT NOT NULL,
-    `Defensive Rating` INT NOT NULL,
-    `Physis Rating` INT NOT NULL,
-    `fk_SpielerID` INT NOT NULL
+CREATE TABLE "Fifa Ranking"(
+    id INT   identity PRIMARY KEY,
+    "Fifa Version" VARCHAR(255) ,
+    "Gesamt Rating" INT ,
+    "Pace Rating" INT ,
+    "Shoot Rating" INT ,
+    "Pasing Rating" INT ,
+    "Dribbling Rating" INT ,
+    "Defensive Rating" INT ,
+    "Physis Rating" INT ,
+    fk_SpielerID INT 
 );
-CREATE TABLE `Mannschaft`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Klubname` VARCHAR(255) NOT NULL,
-    `Klubkürzel` VARCHAR(255) NOT NULL,
-    `Sponsor` VARCHAR(255) NOT NULL,
-    `fk_CoachID` INT NOT NULL
+CREATE TABLE Mannschaft(
+    id INT   identity PRIMARY KEY,
+    Klubname VARCHAR(255) ,
+    Klubkürzel VARCHAR(255) ,
+    Sponsor VARCHAR(255) ,
+    fk_CoachID INT 
 );
-CREATE TABLE `Coach`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `Vorname` VARCHAR(255) NOT NULL,
-    `Nachname` VARCHAR(255) NOT NULL
+CREATE TABLE Coach(
+    id INT   identity PRIMARY KEY,
+    Vorname VARCHAR(255) ,
+    Nachname VARCHAR(255) 
 );
-CREATE TABLE `spielt`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    `von` DATE NOT NULL,
-    `bis` DATE NOT NULL,
-    `gesperrt` TINYINT(1) NOT NULL,
-    `Lohn` DOUBLE(8, 2) NOT NULL,
-    `fk_SpielerID` INT NOT NULL,
-    `fk_MannschaftID` INT NOT NULL
+CREATE TABLE spielt(
+    id INT   identity PRIMARY KEY,
+    von DATE ,
+    bis DATE ,
+    gesperrt BIT ,
+    Lohn FLOAT ,
+    fk_SpielerID INT ,
+    fk_MannschaftID INT 
 );
 ALTER TABLE
-    `Fifa Ranking` ADD CONSTRAINT `fifa ranking_fk_spielerid_foreign` FOREIGN KEY(`fk_SpielerID`) REFERENCES `Spieler`(`id`);
+    "Fifa Ranking" ADD CONSTRAINT fifa_ranking_fk_spielerid_foreign FOREIGN KEY(fk_SpielerID) REFERENCES Spieler(id);
 ALTER TABLE
-    `spielt` ADD CONSTRAINT `spielt_fk_spielerid_foreign` FOREIGN KEY(`fk_SpielerID`) REFERENCES `Spieler`(`id`);
+    spielt ADD CONSTRAINT spielt_fk_spielerid_foreign FOREIGN KEY(fk_SpielerID) REFERENCES Spieler(id);
 ALTER TABLE
-    `Mannschaft` ADD CONSTRAINT `mannschaft_fk_coachid_foreign` FOREIGN KEY(`fk_CoachID`) REFERENCES `Coach`(`id`);
+    Mannschaft ADD CONSTRAINT mannschaft_fk_coachid_foreign FOREIGN KEY(fk_CoachID) REFERENCES Coach(id);
 ALTER TABLE
-    `spielt` ADD CONSTRAINT `spielt_fk_mannschaftid_foreign` FOREIGN KEY(`fk_MannschaftID`) REFERENCES `Mannschaft`(`id`);
+    spielt ADD CONSTRAINT spielt_fk_mannschaftid_foreign FOREIGN KEY(fk_MannschaftID) REFERENCES Mannschaft(id);

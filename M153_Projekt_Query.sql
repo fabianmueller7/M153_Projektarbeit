286655eac67692984ae54597f0921892f6d32e58
Fabian Müller <74955652+fabianmueller7@users.noreply.github.com>
2022-06-06 14:50:03 +0200

    Update M153_Projekt_Query.sql
    Fabian Müller; 5min; Titel der Abfragen hinzugefügt

--- M153_Projekt_Query.sql
+++ M153_Projekt_Query.sql
@@ -1,3 +1,4 @@
+/*Abfrage 1*/
 /*Tabelle mit allen Spielern absteigend nach ihren Fereinsjahren*/
 Select CONCAT(Spieler.Nachname, ' ' , Spieler.Vorname) as 'Spieler', DATEDIFF(year,spielt.von, CURRENT_TIMESTAMP) as 'Jahre im Ferein',  DATEDIFF(year,Spieler.Geburtsdatum, CURRENT_TIMESTAMP) as 'Alter', Mannschaft.Klubname as 'Klub'
 FROM Mannschaft
@@ -5,9 +6,10 @@ join spielt on spielt.fk_MannschaftID = Mannschaft.id
 join Spieler on Spieler.id = spielt.fk_SpielerID
 Order By 'Jahre im Ferein' desc
 
+/*Abfrage 2*/
 /*Tabelle mit allen aktiven Spielern, deren Mannschaften, deren Gesamt Ratings und deren Jahres Lohn.
 Absteigend sortiert nach dem Jahreslohn*/
-Select  CONCAT(Spieler.Nachname, ' ' , Spieler.Vorname) as 'Spieler', [Fifa Ranking].[Gesamt Rating] as "Spieler Wertung", format(spielt.Lohn, 'C2','de-ch') as "Jährliches Gehalt", Mannschaft.Klubkürzel as "Klub"
+Select  CONCAT(Spieler.Nachname, ' ' , Spieler.Vorname) as 'Spieler', [Fifa Ranking].[Gesamt Rating] as "Spieler Wertung", format(spielt.Lohn, 'C2','de-ch') as "JÃ¤hrliches Gehalt", Mannschaft.KlubkÃ¼rzel as "Klub"
 From Spieler
 join spielt on Spieler.id = spielt.fk_SpielerID
 join "Fifa Ranking" on Spieler.id = [Fifa Ranking].fk_SpielerID
@@ -15,28 +17,32 @@ join Mannschaft on Mannschaft.id = spielt.fk_MannschaftID
 Where spielt.bis > CURRENT_TIMESTAMP
 Order By [Fifa Ranking].[Gesamt Rating]  desc
 
-
+/*Abfrage 3*/
 /*Tabelle mit dem Durchschnitt aller Fifa Ranking Gesamt Ratings der Spieler einer Mannschaft*/
-Select Mannschaft.Klubname as 'Klub', AVG([Fifa Ranking].[Gesamt Rating]) as "Durchschnittliche Spieler stärke"
+Select Mannschaft.Klubname as 'Klub', AVG([Fifa Ranking].[Gesamt Rating]) as "Durchschnittliche Spieler stÃ¤rke"
 From Mannschaft
 join spielt on spielt.fk_MannschaftID = Mannschaft.id
 join Spieler on Spieler.id = spielt.fk_SpielerID
 join "Fifa Ranking" on Spieler.id = [Fifa Ranking].fk_SpielerID
 Group by Mannschaft.id, Mannschaft.Klubname
-Order By "Durchschnittliche Spieler stärke" asc
+Order By "Durchschnittliche Spieler stÃ¤rke" asc
 
-/*Lösche Mannschaft Paris Saint-Germain von table Mannschaft*/
+/*Abfrage 4*/
+/*LÃ¶sche Mannschaft Paris Saint-Germain von table Mannschaft*/
 alter table spielt nocheck constraint spielt_fk_mannschaftid_foreign
 DELETE FROM Mannschaft WHERE Mannschaft.Klubname = 'Paris Saint-Germain';
 
-/*Zähle die Anzahl aktiver Spieler einer Mannschaft.*/
+/*Abfrage 5*/
+/*ZÃ¤hle die Anzahl aktiver Spieler einer Mannschaft.*/
 DECLARE @Summe INT = 0;
 EXEC @Summe = sp_AnzSpieler 'RMA';
 PRINT Concat('In der Mannschaft sind ',@Summe, ' Spieler.');
 
-/*Mit hilfe der Stored Procedure sp_UpsertFifaRanking ein Ranking hinzufügen*/
+/*Abfrage 6*/
+/*Mit hilfe der Stored Procedure sp_UpsertFifaRanking ein Ranking hinzufÃ¼gen*/
 EXEC sp_UpsertFifaRanking 'Bruno', 'Fernandes', '1994-09-08', 'testversion', 100, 90,80,70,60,50,40
 
+/*Abfrage 7*/
 /*Mit hilfe der Stored Procedure sp_UpsertFifaRanking ein Ranking akktualisieren*/
 EXEC sp_UpsertFifaRanking 'Bruno', 'Fernandes', '1994-09-08', 'Fifa 22', 100, 90,80,70,60,50,40
 
